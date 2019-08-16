de99c8ea7c59b1ea4a4dd812be14240e60fec4b7
Daniel Franklin <daniel.johann.franklin@gmail.com>
2019-08-15 17:30:59 -0700

    Async Methods
    Added async methods, matching differences, missing differences, and more
    unit tests. Removed unused items.

--- FranklinSoft.RegCompare/FranklinSoft.RegCompare/RegistryCompare.cs
+++ FranklinSoft.RegCompare/FranklinSoft.RegCompare/RegistryCompare.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using FranklinSoft.RegCompare.Models;
 using FranklinSoft.RegCompare.Results;
 using Microsoft.Win32;
-using NLog;
 using RegistryKey = Microsoft.Win32.RegistryKey;
 
 namespace FranklinSoft.RegCompare
 {
     public static class RegistryCompare
     {
+        #region Public Methods
+
         public static RegistryEntriesResult GetRegistryEntries(RegistryHive registryHive, string rootKey, string machineName)
         {
             RegistryKey hive = null;
@@ -40,6 +44,164 @@ namespace FranklinSoft.RegCompare
                 hive?.Close();
             }
         }
+
+        public static List<RegistryEntry> FindMissingRegistryEntries(List<RegistryEntry> list1, List<RegistryEntry> list2)
+        {
+            return list1.Except(list2, new MissingRegistryEntryComparer()).ToList();
+        }
+
+        public static List<RegistryEntryDifference> FindMatchingRegistryEntries(List<RegistryEntry> list1, List<RegistryEntry> list2)
+        {
+            return (from A in list1
+                join B in list2 on A.Name.ToLower() equals B.Name.ToLower()
+                where A.Data != B.Data
+                select new RegistryEntryDifference()
+                {
+                    Name = A.Name,
+                    Location = A.Location,
+                    Type = A.Type,
+                    MachineAData = A.Data,
+                    MachineBData = B.Data
+                }).ToList();
+        }
+
+        public static TestConnectionResult TestConnection(string machineName)
+        {
+            RegistryKey hive = null;
+            try
+            {
+                hive = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, machineName, RegistryView.Registry64);
+
+                return new TestConnectionResult()
+                {
+                    Successful = true
+                };
+            }
+            catch(Exception ex)
+            {
+                return new TestConnectionResult()
+                {
+                    Exception = ex,
+                    Successful = false,
+                    Message =  ex.Message
+                };
+            }
+            finally
+            {
+                hive?.Close();
+            }
+        }
+
+        public static async Task<TestConnectionResult> TestConnectionAsync(string machineName, CancellationTokenSource tokenSource, CancellationToken token, int timeout)
+        {
+            Task<bool> testConnection = Task<bool>.Factory.StartNew(() =>
+            {
+                TestConnectionResult result = TestConnection(machineName);
+                return result.Successful;
+            }, tokenSource.Token);
+
+            TestConnectionResult testConnectionResult = new TestConnectionResult()
+            {
+                Message = string.Empty,
+                Successful = false
+            };
+
+            if (await Task.WhenAny(testConnection, Task.Delay(timeout, token)) == testConnection)
+            {
+                await testConnection;
+                testConnectionResult.Successful = testConnection.Result;
+                return testConnectionResult;
+            }
+
+            if (!testConnection.Result)
+            {
+                try
+                {
+                    tokenSource.Cancel();
+                    token.ThrowIfCancellationRequested();
+                }
+                catch (OperationCanceledException ex)
+                {
+                    testConnectionResult.Exception = ex;
+                    return testConnectionResult;
+                }
+            }
+
+            return testConnectionResult;
+        }
+
+        public static async Task<TestConnectionResult> TestConnectionAsync(string machineName)
+        {
+            Task<bool> testConnection = Task<bool>.Factory.StartNew(() =>
+            {
+                TestConnectionResult result = TestConnection(machineName);
+                return result.Successful;
+            });
+
+            TestConnectionResult testConnectionResult = new TestConnectionResult()
+            {
+                Message = string.Empty,
+                Successful = false
+            };
+
+                await testConnection;
+                testConnectionResult.Successful = testConnection.Result;
+                return testConnectionResult;
+        }
+
+        public static async Task<RegistryEntriesResult> GetRegistryEntriesAsync(RegistryHive registryHive, string rootKey, string machineName)
+        {
+            Task<RegistryEntriesResult> regKeysMachine = Task<RegistryEntriesResult>.Factory.StartNew(() =>
+            {
+                RegistryEntriesResult result = GetRegistryEntries(registryHive, rootKey, machineName);
+                return result;
+            });
+
+            await regKeysMachine;
+            return regKeysMachine.Result;
+        }
+
+        public static async Task<RegistryEntriesResult> GetRegistryEntriesAsync(RegistryHive registryHive, string rootKey, string machineName, CancellationTokenSource tokenSource, CancellationToken token, int timeout)
+        {
+            Task<RegistryEntriesResult> regKeysMachine = Task<RegistryEntriesResult>.Factory.StartNew(() =>
+            {
+                RegistryEntriesResult result = GetRegistryEntries(registryHive, rootKey, machineName);
+                return result;
+            }, tokenSource.Token);
+            if (await Task.WhenAny(regKeysMachine, Task.Delay(timeout, token)) == regKeysMachine)
+            {
+                await regKeysMachine;
+                return regKeysMachine.Result;
+            }
+            else
+            {
+                try
+                {
+                    tokenSource.Cancel();
+                    token.ThrowIfCancellationRequested();
+                }
+                catch (Exception ex)
+                {
+                    return new RegistryEntriesResult()
+                    {
+                        Successful = false,
+                        Exception = ex,
+                        Message = ex.Message,
+                        RegistryEntries = new List<RegistryEntry>()
+                    };
+                }
+            }
+
+            return new RegistryEntriesResult()
+            {
+                Successful = true,
+                RegistryEntries = new List<RegistryEntry>()
+            };
+        }
+
+        #endregion
+
+        #region Private Methods
 
         private static List<RegistryEntry> GetRegistryEntries(string rootKey, RegistryKey hive)
         {
@@ -108,34 +270,7 @@ namespace FranklinSoft.RegCompare
 
             return entries;
         }
-
-        public static TestConnectionResult TestConnection(string machineName)
-        {
-            RegistryKey hive = null;
-            try
-            {
-                hive = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, machineName, RegistryView.Registry64);
-
-                return new TestConnectionResult()
-                {
-                    Successful = true
-                };
-            }
-            catch(Exception ex)
-            {
-                return new TestConnectionResult()
-                {
-                    Exception = ex,
-                    Successful = false
-                };
-            }
-            finally
-            {
-                hive?.Close();
-            }
-        }
-        
+        #endregion
     }
-
 }
 
--- FranklinSoft.RegCompare/FranklinSoft.RegCompareUnitTests/RegCompareUnitTest.cs
+++ FranklinSoft.RegCompare/FranklinSoft.RegCompareUnitTests/RegCompareUnitTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Win32;
 using FranklinSoft.RegCompare;
@@ -21,6 +24,7 @@ namespace FranklinSoft.RegCompareUnitTests
         {
             string machineName = System.Environment.MachineName;
             string rootKey = "Console";
+            RegistryHive hive = RegistryHive.CurrentUser;
             RegistryEntriesResult result = RegistryCompare.GetRegistryEntries(RegistryHive.CurrentUser, rootKey, machineName);
             Assert.IsTrue(result.Successful);
         }
@@ -38,6 +42,52 @@ namespace FranklinSoft.RegCompareUnitTests
         {
             TestConnectionResult result = RegistryCompare.TestConnection("machinea");
             Assert.IsFalse(result.Successful);
+        }
+
+        [TestMethod]
+        public async Task TestConnectionAsyncSuccessTest()
+        {
+            string machineName = System.Environment.MachineName;
+            TestConnectionResult result = await RegistryCompare.TestConnectionAsync(machineName);
+            Assert.IsTrue(result.Successful);
+        }
+
+        [TestMethod]
+        public async Task TestConnectionAsyncFailedTest()
+        {
+            TestConnectionResult result = await RegistryCompare.TestConnectionAsync("machinea");
+            Assert.IsFalse(result.Successful);
+        }
+
+        [TestMethod]
+        public async Task GetRegistryEntriesSuccessAsyncTest()
+        {
+            string machineName = System.Environment.MachineName;
+            string rootKey = "Console";
+            RegistryHive hive = RegistryHive.CurrentUser;
+            RegistryEntriesResult result = await RegistryCompare.GetRegistryEntriesAsync(hive, rootKey, machineName);
+            Assert.IsTrue(result.RegistryEntries.Count > 0, "That key does not have any entries.");
+        }
+
+        [TestMethod]
+        public async Task GetRegistryEntriesFailedAsyncTest()
+        {
+            RegistryEntriesResult result = await RegistryCompare.GetRegistryEntriesAsync(RegistryHive.CurrentUser, "somekey", "machinea");
+            Assert.AreEqual(0, result.RegistryEntries.Count);
+        }
+
+        [TestMethod]
+        public async Task GetRegistryEntriesSuccessAsyncCancellationTest()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            string machineName = System.Environment.MachineName;
+            string rootKey = "Console";
+            RegistryHive hive = RegistryHive.CurrentUser;
+            int timeout = 1;
+            RegistryEntriesResult result = await RegistryCompare.GetRegistryEntriesAsync(hive, rootKey, machineName,tokenSource,token, timeout);
+            Assert.IsFalse(!result.Successful);
         }
     }
 }
